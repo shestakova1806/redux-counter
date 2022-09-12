5925bc951e6382e1dc99d42d2e72e53f6b80cd03
Alexandra Shestakova <shestakova18@bk.ru>
2022-09-12 20:34:14 +0200

    added counter logic

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,33 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button className={classes.button} onClick={decrementHandler}>
+          Decrement
+        </button>
+        <button className={classes.button} onClick={incrementHandler}>
+          Increment
+        </button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
