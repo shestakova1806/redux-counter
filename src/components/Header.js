2253cfeded7ce449e9600d15e9ec59864d8c86f8
Alexandra Shestakova <shestakova18@bk.ru>
2022-09-13 19:34:45 +0200

    added auth logic to the app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import { authActions } from '../store/index';
 import classes from './Header.module.css';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.isAuth);
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
+      {isAuth && <nav>
         <ul>
           <li>
             <a href='/'>My Products</a>
@@ -13,10 +23,10 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
         </ul>
-      </nav>
+      </nav>}
     </header>
   );
 };
