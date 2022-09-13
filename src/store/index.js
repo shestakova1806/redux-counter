b7abf230925a1e5bdd116ad7eae8c1fe95c52ebd
Alexandra Shestakova <shestakova18@bk.ru>
2022-09-13 17:41:48 +0200

    added redux toolkit and created slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,35 @@
 import { legacy_createStore as createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   counter: 0,
   showCounter: true,
 };
 
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
 const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
-    return { 
-      counter: state.counter + 1, 
-      showCounter: state.showCounter 
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
@@ -21,16 +41,16 @@ const counterReducer = (state = initialState, action) => {
   }
 
   if (action.type === 'decrement') {
-    return { 
-      counter: state.counter - 1, 
-      showCounter: state.showCounter 
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === 'toggle') {
-    return { 
-      showCounter: !state.showCounter, 
-      counter: state.counter, 
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
